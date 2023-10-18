54998be8ff5543b0fcef9195bb390a5f8bf824a6
NikolinaTkalec <nikolina.tkalec6@gmail.com>
2023-10-18 09:18:39 +0200

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ParcelModule } from 'single-spa-angular/parcel';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ParcelModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
